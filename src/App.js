1dc507f156d4dbf5cb01e07b035dd32276246aba
Nandha Kumar S <nandha0912002@gmail.com>
2024-06-09 16:47:23 +0530

    Login Particles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import Banner from "./components/Banner";
 import Design from "./components/Design";
-import Header from "./components/Header";
+import LoginPage from "./components/LoginPage"
+import './App.css'
 
 function App() {
   return (
-    <div className="w-full h-screen bg-banner-bg bg-no-repeat bg-center bg-cover relative overflow-hidden">
-      <div className="max-w-6xl mx-auto text-white">
-        {/* Header */}
-        <Header />
-        {/* Banner */}
-        <Banner />
+    <div className="app-container">
+      <div className="text-white">
+        <LoginPage />
       </div>
       <div className="w-full h-screen absolute top-0 left-0">
         <Design />
